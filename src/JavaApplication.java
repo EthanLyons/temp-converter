5289b96b9a338de68bb3e2590683468c765f282f
Ethan Lyons <ethan.lyons84@gmail.com>
2019-01-23 13:46:28 -0700

    Added generated Java source

---
+++ src/JavaApplication.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean repeat;
+        
+        do {
+            double f;
+            double c;
+            
+            f = input.nextDouble();
+            c = (f - 32) * 5 / 9;
+            System.out.println(c);
+            repeat = input.nextBoolean();
+        } while (repeat);
+    }
+}
